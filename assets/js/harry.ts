a15f6201b16c1209363bf1ff4bb24feead151f80
harry-lons <harrylonsdale25@gmail.com>
2024-02-12 18:07:57 -0800

    testing pages

--- assets/js/harry.ts
+++ assets/js/harry.ts
@@ -4,4 +4,7 @@ $(document).ready(function() {
     $('#darkMode').on('click', function() {
         $('.banner, .spotlight, .wrapper').toggleClass('invert')
     }); 
+    $('#generate').on('click',function() {
+        $('#mon1').text('New Text');
+    });
 });
--- assets/js/tsOutput/harry.js
+++ assets/js/tsOutput/harry.js
@@ -4,4 +4,17 @@ $(document).ready(function () {
     $('#darkMode').on('click', function () {
         $('.banner, .spotlight, .wrapper').toggleClass('invert');
     });
+    $('#generate').on('click', function (event) {
+        console.log('clicked!');
+        // Prevent the default button click behavior
+        event.preventDefault();
+
+        // Your code to handle button click goes here
+        generatePlans();
+    });
 });
+
+function generatePlans() {
+    // Your code to generate plans goes here
+    console.log("Generating plans...");
+}
