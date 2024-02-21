3ffd1f5f2c73b9357c335ad6baf37578013d2e54
harry-lons <harrylonsdale25@gmail.com>
2024-02-20 16:47:27 -0800

    finished fp

--- assets/js/harry.ts
+++ assets/js/harry.ts
@@ -26,29 +26,17 @@ $(document).ready(function () {
             return;
         }
         $("#returnMessage").text("Here's a few plans to hit  " + intMileage + "  miles!");
-        if(rest && long){
-            // 6 days, long run
-            generatePlanOne(6,true,intMileage);
-            generatePlanTwo(6,true,intMileage);
-            generatePlanThree(6,true,intMileage);
-        }
-        else if(rest){
-            // 6 days, no long run
-            generatePlanOne(6,false,intMileage);
-            generatePlanTwo(6,false,intMileage);
-            generatePlanThree(6,false,intMileage);
-        }
-        else if(long){
-            // 7 days, long run
-            generatePlanOne(7,true,intMileage);
-            generatePlanTwo(7,true,intMileage);
-            generatePlanThree(7,true,intMileage);
+        if(rest){
+            // 6 days
+            generatePlanOne(6,long,intMileage);
+            generatePlanTwo(6,long,intMileage);
+            generatePlanThree(6,long,intMileage);
         }
         else{
-            // 7 days, no long run
-            generatePlanOne(7,false,intMileage);
-            generatePlanTwo(7,false,intMileage);
-            generatePlanThree(7,false,intMileage);
+            // 7 days 
+            generatePlanOne(7,long,intMileage);
+            generatePlanTwo(7,long,intMileage);
+            generatePlanThree(7,long,intMileage);
         }
 
         function clearTable() {
@@ -126,13 +114,163 @@ $(document).ready(function () {
             $("#fri1").text(res[4]);
             $("#sat1").text(res[5]);
             $("#sun1").text(res[6]);
+            return;
 
         }
-        function generatePlanTwo(_days:number,_long:boolean,_mileage:number){
+        function generatePlanTwo(days:number,long:boolean,mileage:number){
+            // plan two, which aims to balance alternating longer and shorter days
+            console.log("generating plan two");
+            let res:Int32Array = new Int32Array(7);
+            let weights:Float32Array = new Float32Array(7);
+            if(days == 6){
+                // weights[0] and res[0] should already be 0, but this feels like a good practice
+                weights[0] = 0;
+                if(long){
+                    weights[6] = 1/5;
+                    weights[1] = weights[2] = weights[3] = weights[4] = weights[5] = 4/25;
+                }
+                else{
+                    // if no long run, let's change things up a bit from plan 1
+                     weights[1] = weights[3] = weights[5] = 2/9;
+                     weights[2] = weights[4] = weights[6] = 1/9;
+                }
+            }
+            else{
+                if(long){
+                    weights[6] = 1/5;
+                    weights[0] = weights[1] = weights[2] = weights[3] = weights[4] = weights[5] = 1/8;
+                }
+                else{
+                    weights[0] = weights[2] = weights[4] = weights[6] = 1/6;
+                    weights[1] = weights[3] = weights[5] = 1/12;
+                }
+            }
+
+            let miles:number = 0;
+            for(let i = 0; i<7; i++){ 
+                res[i] = weights[i]*mileage;
+                miles += res[i];
+            }
+            let curr = 0;
+            // console.log("miles = " + miles);
+            // console.log("target mileage = " + mileage);
+            
+            while(miles < mileage){
+                // The idea is to spread the remaining mileage across the days (for example, if there are 2 miles
+                // left over, don't put them both on the first day. or on the first 2 days)
+                if(long && curr==6 || rest && curr==0){
+                    // don't add to the long run or the rest day. Do nothing in this case
+                }
+                else{
+                    res[curr % 7]++;
+                    miles++;
+                }
+
+                // If days are odd, increment by 2. If days are even, increment by 3. Then we will cycle through
+                // But if there is a long run, reverse those because there's 1 less day we can actually add to
+                // boolean tomfoolery
+                if((days % 2 == 0 && long) || (days % 2 == 1 && !long)){ curr += 2;}
+                else if((days % 2 == 0 && !long) || (days % 2 == 1 && long)){ curr += 3;}
+                // console.log("miles = " + miles);
+                // console.log("target mileage = " + mileage);
+            }
+
+            $("#mon2").text(res[0]);
+            $("#tue2").text(res[1]);
+            $("#wed2").text(res[2]);
+            $("#thu2").text(res[3]);
+            $("#fri2").text(res[4]);
+            $("#sat2").text(res[5]);
+            $("#sun2").text(res[6]);
             return;
         }
-        function generatePlanThree(_days:number,_long:boolean,_mileage:number){
+        function generatePlanThree(days:number,long:boolean,mileage:number){
+            // Function to generate plan 3, which will use random weights for each day
+            console.log("generating plan three. long = "+long+"days = "+days);
+            let res:Int32Array = new Int32Array(7);
+            let weights:Float32Array = new Float32Array(7);
+            if(days == 6){
+                // weights[0] and res[0] should already be 0, but this feels like a good practice
+                weights[0] = 0;
+                if(long){ 
+                    weights[6] = 1.2;
+                    for(let i = 1; i <= 5; i++){
+                        weights[i] = Math.random();
+                    }
+                    normalize(weights);
+                }
+                else{
+                    for(let i = 1; i <= 6; i++){
+                        weights[i] = Math.random();
+                    }
+                    normalize(weights);
+                }
+            }
+            else{
+                if(long){
+                    weights[6] = 1.2;
+                    for(let i = 0; i <= 5; i++){
+                        weights[i] = Math.random();
+                    }
+                    normalize(weights);
+                }
+                else{
+                    for(let i = 0; i <= 6; i++){
+                        weights[i] = Math.random();
+                    }
+                    normalize(weights);
+                }
+            }
+
+            let miles:number = 0;
+            for(let i = 0; i<7; i++){ 
+                res[i] = weights[i]*mileage;
+                miles += res[i];
+            }
+            let curr = 0;
+            // console.log("miles = " + miles);
+            // console.log("target mileage = " + mileage);
+            
+            while(miles < mileage){
+                // The idea is to spread the remaining mileage across the days (for example, if there are 2 miles
+                // left over, don't put them both on the first day. or on the first 2 days)
+                if(long && curr==6 || rest && curr==0){
+                    // don't add to the long run or the rest day. Do nothing in this case
+                }
+                else{
+                    res[curr % 7]++;
+                    miles++;
+                }
+
+                // If days are odd, increment by 2. If days are even, increment by 3. Then we will cycle through
+                // But if there is a long run, reverse those because there's 1 less day we can actually add to
+                // boolean tomfoolery
+                if((days % 2 == 0 && long) || (days % 2 == 1 && !long)){ curr += 2; }
+                else if((days % 2 == 0 && !long) || (days % 2 == 1 && long)){ curr += 3; }
+                // console.log("miles = " + miles);
+                // console.log("target mileage = " + mileage);
+            }
+
+            $("#mon3").text(res[0]);
+            $("#tue3").text(res[1]);
+            $("#wed3").text(res[2]);
+            $("#thu3").text(res[3]);
+            $("#fri3").text(res[4]);
+            $("#sat3").text(res[5]);
+            $("#sun3").text(res[6]);
             return;
+        }
+        function normalize(weights:Float32Array){
+            let sum = 0;
+            for(let i = 0; i < weights.length; i++){
+                sum += weights[i];
+                console.log("weights["+i+"] = "+ weights[i]);
+            }
+            console.log("sum = "+sum);
+            for(let i = 0; i < weights.length; i++){
+                weights[i] = weights[i]/sum;
+            }
         }
     });
+
 });
