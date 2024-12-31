cf8273a3542f8ccf5338b2fed2c5189f84ee54f2
harry-lons <harrylonsdale25@gmail.com>
2024-12-31 11:20:39 -0800

    home page setup

--- MileCalc/src/App.jsx
+++ MileCalc/src/App.jsx
@@ -1,22 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-// Import your pages
-import Home from './pages/Home';
-import FreshPlan from './pages/FreshPlan';
-import Builder from './pages/Builder';
+import Home from './pages/Home'; // Make sure this path is correct
 
 function App() {
   return (
     <Router>
-      <div id="wrapper">
-        {/* Define routes */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/fp" element={<FreshPlan />} />
-          <Route path="/builder" element={<Builder />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </Router>
   );
 }
